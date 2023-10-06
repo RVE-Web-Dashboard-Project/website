600751162af5f684f9a71a4313a05bfe452d22af
ZRunner <z.runner.mc@gmail.com>
2023-10-06 18:11:03 -0400

    first commit

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,14 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    PUBLIC_URL: string;
+    REACT_APP_API_URL: string;
+  }
+}
+
+declare module "*.svg" {
+  import React = require("react");
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
