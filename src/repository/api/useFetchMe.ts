c3fa0e533657172796a72ebf7cbe7e9e005094f8
ZRunner <z.runner.mc@gmail.com>
2023-10-16 18:20:28 -0400

    feat(auth): add user fetchMe command

--- src/repository/api/useFetchMe.ts
+++ src/repository/api/useFetchMe.ts
@@ -1,18 +1,52 @@
 import { useState } from "react";
 
+import useLogout from "../redux/dispatchs/useLogout";
+import useSetUser from "../redux/dispatchs/useSetUser";
+import useTokenSelector from "../redux/selectors/useTokenSelector";
 import { AuthenticatedUserObject } from "../types/user";
 
 export function useFetchMe() {
-  const [error, _setError] = useState<string | null>(null);
-  const [loading, _setLoading] = useState(false);
-  const [data, _setData] = useState<AuthenticatedUserObject | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<AuthenticatedUserObject | null>(null);
 
-  // const token = useTokenSelector();
-  // const { setUserCommand } = useSetUser();
-  // const { logoutCommand } = useLogout();
+  const token = useTokenSelector();
+  const { setUserCommand } = useSetUser();
+  const { logoutCommand } = useLogout();
 
   async function fetchMeCommand() {
-    // TODO: call the API to fetch the user
+    setLoading(true);
+    if (token === null) {
+      setError("No token provided");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + "/user/me",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      if (response.status === 200) {
+        const json = await response.json() as AuthenticatedUserObject;
+        setData(json);
+        setUserCommand(json);
+      } else if (response.status === 401) {
+        setError("Invalid token");
+        logoutCommand();
+      } else {
+        setError(`Something went wrong: ${response.status} code`);
+      }
+    } catch (err) {
+      setError("Unknown error");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return { fetchMeCommand, error, loading, data };
