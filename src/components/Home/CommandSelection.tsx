ee60dd11ade29412d717d96c7db8dcefe3b41b70
ZRunner <z.runner.mc@gmail.com>
2023-11-06 14:17:50 -0500

    feat(cmds): add parameter textinputs

--- src/components/Home/CommandSelection.tsx
+++ src/components/Home/CommandSelection.tsx
@@ -1,12 +1,13 @@
-import { Box, FormControl, MenuItem, Select, SelectChangeEvent, Typography } from "@mui/material";
+import { Box, FormControl, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
 import { ReactNode, useState } from "react";
 
 import { useGetOrFetchCommands } from "../../repository/commands/useGetOrFetchCommands";
-import { Command } from "../../repository/types/command";
+import { Command, CommandParameter } from "../../repository/types/command";
 
 export const CommandSelection = () => {
   const { commands, error } = useGetOrFetchCommands();
   const [selectedCommand, setSelectedCommand] = useState<Command | null>(null);
+  const [parameters, setParameters] = useState<Record<string, number> | null>(null);
 
   if (error) {
     return <Typography variant="h6" color="error">{error}</Typography>;
@@ -16,6 +17,11 @@ export const CommandSelection = () => {
     const commandId = event.target.value as string;
     const command = commands.find((cmd) => cmd.id === Number(commandId));
     setSelectedCommand(command ?? null);
+  };
+
+  const onParameterValueChange = (parameterId: number, value: number) => {
+    const absoluteParamId = `${selectedCommand?.id}-${parameterId}`;
+    setParameters((prev) => ({ ...prev, [absoluteParamId]: value }));
   };
 
   return (
@@ -32,8 +38,59 @@ export const CommandSelection = () => {
             <MenuItem key={command.id} value={command.id}>{command.name}</MenuItem>
           ))}
         </Select>
+
+        {selectedCommand?.parameters.map((parameter) => (
+          <ParameterTextField
+            key={`${selectedCommand?.id}-${parameter.id}`}
+            parameter={parameter}
+            value={parameters?.[`${selectedCommand?.id}-${parameter.id}`]}
+            onChange={onParameterValueChange}
+          />
+        ))
+        }
+
       </FormControl>
     </Box>
   );
+};
 
+
+interface ParameterTextFieldProps {
+  parameter: CommandParameter;
+  value?: number;
+  onChange: (parameterId: number, value: number) => void;
+}
+
+const ParameterTextField = ({ parameter, value, onChange }: ParameterTextFieldProps) => {
+  const [currentTextValue, setCurrentTextValue] = useState((value ?? parameter.default).toString());
+
+  const onValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentTextValue(e.target.value);
+    if (checkValue(e.target.value)) {
+      console.log("changing to", Number(e.target.value));
+      onChange(parameter.id, Number(e.target.value));
+    }
+  };
+
+  function checkValue(inputValue: string) {
+    if (parameter.type === "int") {
+      return Number.isInteger(Number(inputValue));
+    } else {
+      return !Number.isNaN(Number(inputValue));
+    }
+  }
+
+  return (
+    <TextField
+      id={`command-parameter-${parameter.id}`}
+      inputMode="numeric"
+      label={parameter.name}
+      value={currentTextValue}
+      onChange={onValueChange}
+      size="small"
+      color="secondary"
+      error={!checkValue(currentTextValue)}
+      required
+    />
+  );
 };
