38a58f58c0893989d2e015c987bcec9834b7e67b
ZRunner <z.runner.mc@gmail.com>
2023-11-17 14:05:49 -0500

    feat(invite): implement password strength check

---
+++ src/repository/commands/useCheckPasswordStrength.ts
@@ -0,0 +1,59 @@
+import { useMemo } from "react";
+
+export enum PasswordChecks {
+  HAS_NUMBER = "HAS_NUMBER",
+  HAS_UPPERCASE = "HAS_UPPERCASE",
+  HAS_LOWERCASE = "HAS_LOWERCASE",
+  HAS_SPECIAL_CHAR = "HAS_SPECIAL_CHAR",
+  IS_12_CHARS = "IS_12_CHARS",
+}
+
+export const PasswordChecksLabels = {
+  [PasswordChecks.HAS_NUMBER]: "Password must contain at least one number",
+  [PasswordChecks.HAS_UPPERCASE]: "Password must contain at least one uppercase letter",
+  [PasswordChecks.HAS_LOWERCASE]: "Password must contain at least one lowercase letter",
+  [PasswordChecks.HAS_SPECIAL_CHAR]: "Password must contain at least one special character",
+  [PasswordChecks.IS_12_CHARS]: "Password must be at least 12 characters long",
+};
+
+const checks = [
+  {
+    name: PasswordChecks.HAS_NUMBER,
+    regex: /\d+/,
+  },
+  {
+    name: PasswordChecks.HAS_UPPERCASE,
+    regex: /[A-Z]+/,
+  },
+  {
+    name: PasswordChecks.HAS_LOWERCASE,
+    regex: /[a-z]+/,
+  },
+  {
+    name: PasswordChecks.HAS_SPECIAL_CHAR,
+    regex: /[!@#$%^&*()]+/,
+  },
+  {
+    name: PasswordChecks.IS_12_CHARS,
+    regex: /.{21,}/,
+  },
+];
+
+export default function useCheckPasswordStrength(password: string) {
+  const executedChecks = useMemo(() => (
+    checks.map((check) => ({
+      name: check.name,
+      passed: check.regex.test(password),
+    }))
+  ), [password]);
+
+  const failedChecks = executedChecks.filter((check) => !check.passed);
+
+  const isStrong = failedChecks.length === 0;
+
+  return {
+    executedChecks,
+    isStrong,
+    failedChecks,
+  };
+}
