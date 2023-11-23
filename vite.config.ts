9e415ad5a44eccc535ba07fb0753df23522aa98f
ZRunner <z.runner.mc@gmail.com>
2023-11-23 16:56:48 -0500

    feat(build): replace CRA with Vite

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from 'vite';
+
+
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
+  console.debug(process.env.PORT)
+  return {
+    // depending on your application, base can also be "/"
+    base: "",
+    plugins: [react(), viteTsconfigPaths()],
+    server: {
+      // this ensures that the browser opens upon server start
+      open: true,
+      // this sets a default port to 3000
+      port: Number(process.env.PORT) || 3000,
+    },
+  }
+});
