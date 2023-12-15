f33f56683364d6694ede2a952fae7794eec3b964
ZRunner <z.runner.mc@gmail.com>
2023-12-15 15:34:45 +0100

    feat(admin): add invitations list

---
+++ src/components/ManageUsers/InvitationsList.tsx
@@ -0,0 +1,46 @@
+import { CircularProgress, Divider, List, Stack, Typography } from "@mui/material";
+import { Fragment } from "react";
+
+import { useFetchInvitations } from "../../repository/api/useFetchInvitations";
+import { InvitationListItem } from "./InvitationListItem";
+
+export const InvitationList = () => {
+  const { data: invitations, error, loading, fetchInvitationsCommand } = useFetchInvitations();
+
+  if (!invitations && !loading && !error) {
+    fetchInvitationsCommand();
+  }
+
+  const Content = () => {
+    if (error) {
+      return <Typography color="error">{error}</Typography>;
+    }
+    if (!invitations) {
+      return <CircularProgress color="secondary" />;
+    }
+    const sortedInvitations = invitations.sort((a, b) => a.createdAt.localeCompare(b.createdAt));
+
+    return (
+      <List >
+        {sortedInvitations.map((invite, index) => (
+          <Fragment key={invite.id}>
+            {(index !== 0) && <Divider />}
+            <InvitationListItem
+              invite={invite}
+            />
+          </Fragment>
+        ))}
+      </List>
+    );
+  };
+
+  return (
+    <Stack flex={1}>
+      <Typography variant="h5" noWrap>
+      Invitation list
+      </Typography>
+
+      <Content />
+    </Stack>
+  );
+};
