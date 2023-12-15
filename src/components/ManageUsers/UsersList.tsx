b30cf93ccd5abc190e204fde43fc23a57c7c39a8
ZRunner <z.runner.mc@gmail.com>
2023-12-15 15:15:00 +0100

    feat(admin): add users list

---
+++ src/components/ManageUsers/UsersList.tsx
@@ -0,0 +1,66 @@
+import { CircularProgress, Divider, List, Stack, Typography } from "@mui/material";
+import { Fragment } from "react";
+
+import { useFetchUsers } from "../../repository/api/useFetchUsers";
+import { useGetOrFetchMe } from "../../repository/commands/useGetOrFetchMe";
+import { UserObject } from "../../repository/types/user";
+import { UserListItem } from "./UserListItem";
+
+function canDeleteUser(user: UserObject, { me, userList }: {me: UserObject, userList: UserObject[]}) {
+  // check if user is not me
+  if (user.id === me.id) {
+    return false;
+  }
+  // check if user is not the only user
+  if (userList.length === 1) {
+    return false;
+  }
+  // check if user is not the only admin
+  if (user.isAdmin && userList.filter((u) => u.isAdmin).length === 1) {
+    return false;
+  }
+  return true;
+}
+
+export const UsersList = () => {
+  const { user: authenticatedUser } = useGetOrFetchMe();
+  const { data: users, error, loading, fetchUsersCommand } = useFetchUsers();
+
+  if (!users && !loading && !error) {
+    fetchUsersCommand();
+  }
+
+  const Content = () => {
+    if (error) {
+      return <Typography color="error">{error}</Typography>;
+    }
+    if (!users || !authenticatedUser) {
+      return <CircularProgress color="secondary" />;
+    }
+    const sortedUsers = users.sort((a, b) => a.name.localeCompare(b.name));
+
+    return (
+      <List >
+        {sortedUsers.map((user, index) => (
+          <Fragment key={user.id}>
+            {(index !== 0) && <Divider />}
+            <UserListItem
+              user={user}
+              canDelete={canDeleteUser(user, { me: authenticatedUser, userList: users })}
+            />
+          </Fragment>
+        ))}
+      </List>
+    );
+  };
+
+  return (
+    <Stack flex={1}>
+      <Typography variant="h5" noWrap>
+      User list
+      </Typography>
+
+      <Content />
+    </Stack>
+  );
+};
