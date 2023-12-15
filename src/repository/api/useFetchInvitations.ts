a7593591f9983c3e55ad214858261f3af9423847
ZRunner <z.runner.mc@gmail.com>
2023-12-15 15:34:38 +0100

    feat(api): add method to fetch invitations list

---
+++ src/repository/api/useFetchInvitations.ts
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import useTokenSelector from "../redux/selectors/useTokenSelector";
+import { InvitationInfo } from "../types/user";
+
+type ApiResponse = InvitationInfo[];
+
+export function useFetchInvitations() {
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<InvitationInfo[] | null>(null);
+
+  const token = useTokenSelector();
+
+  async function fetchInvitationsCommand() {
+    setLoading(true);
+    setError(null);
+
+    if (token === null) {
+      setError("No token provided");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + "/invitation",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+      if (response.status === 200) {
+        const json = await response.json() as ApiResponse;
+        setData(json);
+      } else if (response.status === 401) {
+        setError("Invalid token");
+      } else {
+        setError(`Something went wrong: code ${response.status}`);
+      }
+    } catch (err) {
+      setError("Unknown error");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { error, loading, data, fetchInvitationsCommand };
+}
