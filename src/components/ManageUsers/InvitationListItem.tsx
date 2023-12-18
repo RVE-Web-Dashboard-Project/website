969225c5031b3002a2ae00bedde7d1758c24805e
ZRunner <z.runner.mc@gmail.com>
2023-12-18 14:39:40 +0100

    feat(admin): call API to delete invite

--- src/components/ManageUsers/InvitationListItem.tsx
+++ src/components/ManageUsers/InvitationListItem.tsx
@@ -1,5 +1,6 @@
 import { ListItem, ListItemSecondaryAction, ListItemText, Stack } from "@mui/material";
 
+import { useDeleteInvitation } from "../../repository/api/useDeleteInvitation";
 import { InvitationInfo } from "../../repository/types/user";
 import { DeleteButton } from "./DeleteBtn";
 
@@ -7,21 +8,36 @@ interface InvitationListItemProps {
   invite: InvitationInfo
 }
 
-export const InvitationListItem = ({ invite }: InvitationListItemProps) => (
-  <ListItem sx={{ pl: 0 }}>
-    <ListItemText
-      primary={
-        <Stack direction="row" spacing={1} alignItems="center" useFlexGap>
-          {invite.username}
-        </Stack>
-      }
-      secondary={<InvitationSecondaryInfo invite={invite} />}
-    />
-    <ListItemSecondaryAction>
-      <DeleteButton onClick={console.log} tooltip="Delete invitation" />
-    </ListItemSecondaryAction>
-  </ListItem>
-);
+export const InvitationListItem = ({ invite }: InvitationListItemProps) => {
+  const { deleteInvitation, loading } = useDeleteInvitation();
+
+  function handleDelete() {
+    console.log("Deleting invitation", invite.id);
+    deleteInvitation(invite.id);
+  }
+
+  return (
+    <ListItem sx={{ pl: 0 }}>
+      <ListItemText
+        primary={
+          <Stack direction="row" spacing={1} alignItems="center" useFlexGap>
+            {invite.username}
+          </Stack>
+        }
+        secondary={<InvitationSecondaryInfo invite={invite} />}
+      />
+      <ListItemSecondaryAction>
+        <DeleteButton
+          onClick={handleDelete}
+          tooltip="Delete invitation"
+          loading={loading}
+          confirmTitle={`Delete ${invite.username} invitation`}
+          confirmMessage="Are you sure you want to delete this invitation?"
+        />
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
 
 const InvitationSecondaryInfo = ({ invite }: InvitationListItemProps) => {
   const creationDateLabel = "Created at " + new Date(invite.createdAt).toLocaleString("en", {
