7ed56c9c50bea6aaf7a98132dac812898ef8f584
ZRunner <z.runner.mc@gmail.com>
2024-02-13 18:16:38 -0500

    fix(useOngoingCommandsSelector): simplify selector logic

--- src/repository/redux/selectors/useOngoingCommandsSelector.ts
+++ src/repository/redux/selectors/useOngoingCommandsSelector.ts
@@ -1,13 +1,5 @@
-import { createSelector } from "@reduxjs/toolkit";
-
 import { useAppSelector } from "../hooks";
-import { RootState } from "../store";
-
-const ongoingCommandsSelector = createSelector(
-  (state: RootState) => state.commands.ongoingCommands,
-  (ongoingCommands) => ongoingCommands,
-);
 
 export default function useOngoingCommandsSelector() {
-  return useAppSelector(ongoingCommandsSelector);
+  return useAppSelector((state) => state.commands.ongoingCommands);
 }
